995f943d00612e178c86a5fe7eb91f3691c61bc7
Ahmad Al Gailani <dull.x22@gmail.com>
2022-07-27 00:14:43 -0400

    Created User entity class (for postgres)

---
+++ src/repo/User.ts
@@ -0,0 +1,50 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Length } from "class-validator";
+
+@Entity({name: "Users"})
+export class User{
+    @PrimaryGeneratedColumn({name:"id", type:"bigint"})
+    id: string;
+
+    @Column("varchar", {
+        name: "Email",
+        length: 120,
+        unique:true,
+        nullable: false,
+    })
+    email: string;
+
+    @Column("varchar", {
+        name: "UserName",
+        length: 60,
+        unique:true,
+        nullable: false,
+    })
+    userName: string;
+
+    @Column("varchar", {
+        name: "Password",
+        length: 100,
+        nullable: false,
+    })
+    @Length(8,100)
+    password: string;
+
+    @Column("boolean", {
+        name: "Confirmed",
+        default: false,
+        nullable: false,
+    })
+    confirmed: boolean;
+
+    @Column("boolean", {
+        name: "IsDisabled",
+        default: false,
+        nullable: false,
+    })
+    isDisabled: boolean;
+
+
+
+
+}
